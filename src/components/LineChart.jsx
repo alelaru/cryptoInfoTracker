93c6e3ee2fa8f6903bb605404c826fa1dc926402
Alejandro Lara <alelaru@gmail.com>
2021-10-10 22:21:40 +0200

    CryptoDetails page almost done, we are missing the chart for price

---
+++ src/components/LineChart.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Line} from "react-chartjs-2"
+import { Typhography } from "antd"
+
+// const {Title} = Typhography
+
+const LineChart = () => {
+    return (
+        
+        <div>
+            
+        </div>
+    )
+}
+
+export default LineChart
